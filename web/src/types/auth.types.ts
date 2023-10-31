a9dc0cf81c1342738b8a34f57ed3a40113a667b3
ErikPervious <erickmatheus3000@gmail.com>
2023-10-31 03:55:14 -0300

    Add credit card information to userInformations interface

--- web/src/types/auth.types.ts
+++ web/src/types/auth.types.ts
@@ -4,8 +4,31 @@ export interface userInformations {
   username: string;
   agency: number;
   balance: number;
+  creditCard: {
+    number: number;
+    cvv: number;
+    expirationDate: string;
+    availableCredit: number;
+    totalCredit: number;
+    debt: number; 
+  };
   account: number;
   createdAt: Timestamp;
   isActive: boolean;
+  totalWithdraws: number;
+  totalDeposits: number;
+  totalTransfers: number;
+  historicOfBalance: {
+    value: number;
+    date: Timestamp;
+  }[];
+  historicOfMovements: {
+    value: number;
+    sender?: string, // user uid
+    destination?: string, // user uid
+    date: Timestamp,
+    type: 'transfer@sent' | 'transfer@received' | 'deposit' | 'withdraw' | 'credit@payment' | 'credit@loan';
+  }[];
+  lastLogin: Timestamp;
 };
 
