48b4700c76412112937e702b9230a7e13f742be6
ErikPervious <erickmatheus3000@gmail.com>
2023-11-16 21:34:14 -0300

    Add AuthSec test for user account access

---
+++ web/src/tests/AuthSec.test.ts
@@ -0,0 +1,35 @@
+import 'isomorphic-fetch';
+import { auth, db } from '../services/firebase';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, connectAuthEmulator } from "firebase/auth";
+import { doc, getDoc } from 'firebase/firestore';
+
+describe("User", () => {   
+  beforeAll(async () => {
+    connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+  });
+
+  const email1 = "pervious@gmail.com";
+  const password1 = "pervioussenha";
+
+  const email2 = "hacker@gmail.com";
+  const password2 = "hackersenha";
+
+  it("cannot access another user's account using UID", async () => {
+    // Create two users
+    const newUserCredential = await createUserWithEmailAndPassword(auth, email1, password1);
+    const user = newUserCredential.user;
+
+    await auth.signOut();
+    const anotherUserCredential = await createUserWithEmailAndPassword(auth, email2, password2);
+    const userHacker = anotherUserCredential.user;
+    
+    // Try to access the user account using uid, expect to fail
+    const userDoc = doc(db, 'users', user?.uid);
+    const userDocHacker = doc(db, 'users', userHacker?.uid);
+
+    expect(getDoc(userDoc)).rejects.toThrow();
+    expect(getDoc(userDocHacker)).rejects.not.toThrow();
+  });
+
+});
+
