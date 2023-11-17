bc75eedfbb78d07cab0a66e9482eed2c4f58883a
ErikPervious <erickmatheus3000@gmail.com>
2023-11-16 21:34:06 -0300

    Refactor oAuth.test.ts for system authentication

--- web/src/tests/oAuth.test.ts
+++ web/src/tests/oAuth.test.ts
@@ -1,14 +1,15 @@
 import 'isomorphic-fetch';
-import firebaseApp, { auth } from '../services/firebase';
+import { auth } from '../services/firebase';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, connectAuthEmulator } from "firebase/auth";
 
-describe("User", () => {   
+describe("Testing System Auth", () => {   
   beforeAll(async () => {
     connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+    await auth.signOut();
   });
 
-  const email = "opaan@example.com";
-  const password = "testPassword";
+  const email = "pervious@gmail.com";
+  const password = "senhadificil";
 
   it("can register", async () => {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
@@ -17,15 +18,16 @@ describe("User", () => {
     expect(user?.email).toEqual(email);
   });
 
-  it("can login", async () => {
-    await auth.signOut();
+  it("can login and get last access time", async () => {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
     expect(user).not.toBeNull();
-    expect(user?.email).toBe(email);
+    expect(user?.email).toEqual(email);
+    expect(user?.metadata.lastSignInTime).not.toBeNull();
   });
 
-  it("can logout", async () => {
+  it("can logout and delete session", async () => {
+    await signInWithEmailAndPassword(auth, email, password);
     await auth.signOut();
     const user = auth.currentUser;
     expect(user).toBeNull();
